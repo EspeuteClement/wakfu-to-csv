13d2b63455e49afc79296f9417a2430e0cdeb869
Valden <espeut.clement@gmail.com>
2023-11-13 22:47:26 +0100

    added more stats

--- wakfu-stuff.py
+++ wakfu-stuff.py
@@ -44,27 +44,49 @@ def load_data_page(version, page):
 
 params = {
     20: {'name': "PV", 'formula': [0]},
+    21: {'name': "PV", 'formula': [-1]},
+
     31: {'name': "PA", 'formula': [0]},
     41: {'name': "PM", 'formula': [0]},
     160: {'name': "PO", 'formula': [0]},
     191: {'name': "PW", 'formula': [0]},
+    192: {'name': "PW", 'formula': [-1]},
+    194: {'name': "PW", 'formula': [-1]},
+
+
+    56: {'name': "PA", 'formula': [-1]},
+    
+    57: {'name': "PM", 'formula': [-1]},
+
+
+    173: {'name': "Tacle", 'formula': [0]},
+    174: {'name': "Tacle", 'formula': [-1]},
+
+    176: {'name': "Esquive", 'formula': [0]},
+    177: {'name': "Esquive", 'formula': [-1]},
 
 
     26: {'name': "Maitrise soin", 'formula': [0]},
     149: {'name': "Maitrise CC", 'formula': [0]},
+
     150: {'name': "CC", 'formula': [0]},
+    168: {'name': "CC", 'formula': [-1]},
 
     180: {'name': "Maitrise Dos", 'formula': [0]},
+    181: {'name': "Maitrise Dos", 'formula': [-1]},
+
+    875: {'name': "Parade", 'formula': [0]},
+    876: {'name': "Parade", 'formula': [-1]},
+
+
+
     1052: {'name': "Maitrise Mélee", 'formula': [0]},
     1053: {'name': "Maitrise Distance", 'formula': [0]},
     1055: {'name': "Maitrise Berserk", 'formula': [0]},
 
-
-
     -1: {'name': "Maitrise Elem"},
     -2: {'name': "NB Elements"},
 }
-
 
 def parse_data(data):
     with open("out.csv", "wb") as file:
@@ -73,8 +95,14 @@ def parse_data(data):
         line += "Level;"
         line += "Raretée;"
         line += "Type;"
+
+        param_names = set()
         for p in params:
-            line += params[p]["name"] + ";"
+            param_names.add(params[p]["name"])
+
+        for p in param_names:
+            line += p + ";"
+
         line += "\n"
         
         file.write(line.encode('utf8'))
@@ -99,25 +127,29 @@ def parse_data(data):
 
 
                 effects = {}
-                for k in params:
-                    effects[k] = 0
+                for p in param_names:
+                    effects[p] = 0
                 
                 for effect in value["definition"]["equipEffects"]:
                     effect = effect["effect"]
                     t = effect["definition"]["actionId"]
                     # maitrise multi elem
                     if t == 1068:
-                        effects[-1] = int(effect["definition"]["params"][0] + level * effect["definition"]["params"][1])
-                        effects[-2] = int(effect["definition"]["params"][2] + level * effect["definition"]["params"][3])
+                        effects["Maitrise Elem"] = int(effect["definition"]["params"][0] + level * effect["definition"]["params"][1])
+                        effects["NB Elements"] = int(effect["definition"]["params"][2] + level * effect["definition"]["params"][3])
                     elif t == 120:
-                        effects[-1] = int(effect["definition"]["params"][0] + level * effect["definition"]["params"][1])
-                        effects[-2] = 4
+                        effects["Maitrise Elem"] = int(effect["definition"]["params"][0] + level * effect["definition"]["params"][1])
+                        effects["NB Elements"] = 4
                     elif t in params:
                         tt = params[t]
                         v = 0
                         for f in tt["formula"]:
-                            v += effect["definition"]["params"][0 + f * 2] + level * effect["definition"]["params"][1 + f * 2] 
-                        effects[t] = int(v)
+                            mult = 1
+                            if f < 0:
+                                f = f * -1 - 1
+                                mult = -1
+                            v += mult * (effect["definition"]["params"][0 + f * 2] + level * effect["definition"]["params"][1 + f * 2]) 
+                        effects[tt["name"]] = int(v)
 
                 for effect in effects:
                     line += str(effects[effect]) + ";" 
